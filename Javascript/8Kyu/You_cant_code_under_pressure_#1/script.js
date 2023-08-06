f8623cdd83e0ea3edce4734e352d2caa95e73d05
Chrystalio <chrystalio.k@outlook.com>
2023-08-06 18:40:33 +0700

    8 Kyu You cant code under pressure

---
+++ Javascript/8Kyu/You_cant_code_under_pressure_#1/script.js
@@ -0,0 +1,19 @@
+/* Instructions
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+// Solutions
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i*2;
+}
+
+/* Explained Solutions
+
+This function takes an integer as input (i), inside the function there's a single return statement.
+The return statement multiplies the input integer i by 2 using the * operator.
+The doubled value of the integer is then returned as the output.
+So, the function doubleInteger takes an integer as input and returns its doubled value as the output.
+
+*/
